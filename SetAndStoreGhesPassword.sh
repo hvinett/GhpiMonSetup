fac5a7632b2f5256bb012449f28aa52f108e5b97
Vineeth Hanumanth <39301309+hvinett@users.noreply.github.com>
2020-07-30 15:10:06 +0530

    Create SetAndStoreGhesPassword.sh

---
+++ SetAndStoreGhesPassword.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+    case "$key" in
+    --keyVault)  
+        keyVault=$2
+        shift
+        shift
+        echo "keyVault: $keyVault"
+        ;;
+    --vmManagedUserId)  
+        vmManagedUserId=$2
+        shift
+        shift
+        echo "vmManagedUserId: $vmManagedUserId"
+        ;;
+     *)
+        echo "Invalid parameter: $1"
+        exit 1
+        ;;
+    esac
+done
+$GhesAdminPassword=$(openssl rand -base64 32)
+az login --identity --username $vmManagedUserId
+az keyvault secret set --vault-name $keyVault --name "GhesAdminPassword" --value "$GhesAdminPassword"
