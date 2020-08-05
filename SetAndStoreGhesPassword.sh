f2dae767ce208594b8e4e81cced49888718ed294
Vineeth Hanumanth <39301309+hvinett@users.noreply.github.com>
2020-08-05 12:51:31 +0530

    added installing azure cli

--- SetAndStoreGhesPassword.sh
+++ SetAndStoreGhesPassword.sh
@@ -16,17 +16,13 @@ key="$1"
         shift
         echo "vmManagedUserId: $vmManagedUserId"
         ;;
-    --ghesAdminPassword)  
-        ghesAdminPassword=$2
-        shift
-        shift
-        echo "ghesAdminPassword: $ghesAdminPassword"
-        ;;
      *)
         echo "Invalid parameter: $1"
         exit 1
         ;;
     esac
 done
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+ghesAdminPassword=$(openssl rand -base64 14)
 az login --identity --username $vmManagedUserId
 az keyvault secret set --vault-name $keyVault --name "GhesAdminPassword" --value "$ghesAdminPassword"
